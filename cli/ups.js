c349934e35e445cb40436fe8323058de2e044aa1
ebaauw <erik.baauw@xs4all.nl>
2023-01-01 09:30:33 +0100

    Happy New Year

--- cli/ups.js
+++ cli/ups.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 // homebridge-ups/cli/ups.js
-// Copyright © 2022 Erik Baauw. All rights reserved.
+// Copyright © 2022-2023 Erik Baauw. All rights reserved.
 //
 // Homebridge plugin for UPS.
 //
